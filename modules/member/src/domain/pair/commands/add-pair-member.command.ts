bb22b80bebb2e13d7d833d49dbe1598bb3e51760
河本 勇人 <kawamoto.web@gmail.com>
2023-06-17 08:51:33 +0900

    ✨ AddPairMemberCommand の実装（WIP）

---
+++ modules/member/src/domain/pair/commands/add-pair-member.command.ts
@@ -0,0 +1,25 @@
+import { PairRepository } from '@domain/pair/interfaces'
+import { ParticipantId } from '@domain/participant/models'
+import { TeamId } from '@domain/team/models'
+import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs'
+
+export class AddPairMemberCommand {
+  constructor(public readonly teamId: TeamId, public readonly newMemberId: ParticipantId) {}
+}
+
+@CommandHandler(AddPairMemberCommand)
+export class AddPairMemberCommandHandler implements ICommandHandler<AddPairMemberCommand> {
+  constructor(
+    private readonly pairRepository: PairRepository,
+    private readonly openPairFinder: OpenPairFinder,
+    private readonly eventBus: EventBus,
+  ) {}
+
+  async execute(command: AddPairMemberCommand) {
+    const openPair = this.openPairFinder.find(command.teamId)
+    const [addedPair, pairMemberAdded] = openPair.addMember(command.newMemberId)
+
+    await this.pairRepository.store(addedPair, pairMemberAdded)
+    this.eventBus.publish(pairMemberAdded)
+  }
+}
