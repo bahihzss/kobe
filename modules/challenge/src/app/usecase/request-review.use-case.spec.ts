25eb0cf4a26bb542712fe110353f6a06a399fcf3
河本 勇人 <kawamoto.web@gmail.com>
2023-06-26 06:42:01 +0900

    ✨ RequestReviewUseCase の結合テストを実装

---
+++ modules/challenge/src/app/usecase/request-review.use-case.spec.ts
@@ -0,0 +1,51 @@
+import { RequestReviewUseCase } from '@app/usecase/request-review.use-case'
+import { PrismaSeeder } from '@infra/prisma.seeder'
+import { PrismaService } from '@infra/prisma.service'
+import { ProgressPrismaRepository } from '@infra/progress.prisma.repository'
+import { Test, TestingModule } from '@nestjs/testing'
+import { Token } from '@root/token'
+
+describe('RequestReviewUseCase', () => {
+  let requestReviewUseCase: RequestReviewUseCase
+  let prisma: PrismaService
+
+  beforeEach(async () => {
+    const testApp = await Test.createTestingModule({
+      providers: [
+        PrismaService,
+        PrismaSeeder,
+        {
+          provide: Token.ProgressRepository,
+          useClass: ProgressPrismaRepository,
+        },
+        RequestReviewUseCase,
+      ],
+    }).compile()
+
+    await testApp.get(PrismaSeeder).seed()
+
+    requestReviewUseCase = testApp.get(RequestReviewUseCase)
+    prisma = testApp.get(PrismaService)
+  })
+
+  test('課題と参加者を指定すると、課題のステータスがレビュー中になる', async () => {
+    const challengeId = PrismaSeeder.data.challenge.id
+    const assigneeId = PrismaSeeder.data.participant.id
+
+    await requestReviewUseCase.execute({
+      challengeId,
+      assigneeId,
+    })
+
+    const progress = await prisma.progress.findUnique({
+      where: {
+        assigneeId_challengeId: {
+          assigneeId,
+          challengeId,
+        },
+      },
+    })
+
+    expect(progress?.status).toBe('IN_REVIEW')
+  })
+})
--- modules/challenge/src/app/usecase/request-review.use-case.ts
+++ modules/challenge/src/app/usecase/request-review.use-case.ts
@@ -1,14 +1,21 @@
 import { ChallengeId } from '@domain/challenge'
 import { ParticipantId } from '@domain/participant'
 import { ProgressRepository } from '@domain/progress'
+import { UseCaseException } from '@kobe/common/app/error/use-case-exception.error'
+import { Inject, Injectable } from '@nestjs/common'
+import { Token } from '@root/token'
 
 interface RequestReviewUseCaseParams {
   challengeId: string
   assigneeId: string
 }
 
+@Injectable()
 export class RequestReviewUseCase {
-  constructor(private readonly progressRepository: ProgressRepository) {}
+  constructor(
+    @Inject(Token.ProgressRepository)
+    private readonly progressRepository: ProgressRepository,
+  ) {}
 
   async execute(params: RequestReviewUseCaseParams): Promise<void> {
     const challengeId = new ChallengeId(params.challengeId)
@@ -18,6 +25,11 @@ export class RequestReviewUseCase {
       challengeId,
       assigneeId,
     })
+
+    if (!progress) {
+      throw new UseCaseException('指定された課題の進捗が見つかりませんでした。')
+    }
+
     const requestedProgress = progress.requestReview({ operatorId: assigneeId })
     await this.progressRepository.save(requestedProgress)
   }
